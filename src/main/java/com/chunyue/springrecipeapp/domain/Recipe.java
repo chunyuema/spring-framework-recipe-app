8e11bd316af47f30d5680ddc9a3707be12d90717
Chunyue Ma <cma01@wesleyan.edu>
2021-09-28 22:50:13 -0400

    create one to many relationship of recipe and ingredients

--- src/main/java/com/chunyue/springrecipeapp/domain/Recipe.java
+++ src/main/java/com/chunyue/springrecipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.chunyue.springrecipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -14,6 +15,9 @@ public class Recipe {
     private String url;
     private String directions;
     // todo: add difficulty
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -91,5 +95,13 @@ public class Recipe {
 
     public void setNote(Notes note) {
         this.note = note;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
